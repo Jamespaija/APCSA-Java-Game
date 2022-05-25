92238289cc42774dca9111f8cddf93927cde1bd7
Jamespaija <jpaijapun@gmail.com>
2022-05-25 12:12:47 -0400

    target and score

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,12 @@
+
 /* Game Class Starter File
  * Last Edit: 5/6/2021
  * Author: James Paija Pun
  */
 import java.util.Random;
+
+import javax.naming.TimeLimitExceededException;
+
 public class Game {
 
   private Grid grid;
@@ -10,11 +14,15 @@ public class Game {
   private int msElapsed;
   private int timesGet;
   private int timesAvoid;
-  private String userPic = "images/target.png";
-  /*private final String fileCursorLocked = "images/cursors/locked.png",
-			fileCursorUnlocked = "images/cursors/unlocked.png",
-			fileCursorDefault = "images/cursors/default.png";*/
-  //public int [][]lastLoc =  int [][]; //not valid syntax, what is this for?
+  private String targetPic = "images/target.png";
+  /*
+   * private final String fileCursorLocked = "images/cursors/locked.png",
+   * fileCursorUnlocked = "images/cursors/unlocked.png",
+   * fileCursorDefault = "images/cursors/default.png";
+   */
+  // public int [][]lastLoc = int [][]; //not valid syntax, what is this for?
+  // public int expireTime = Grid.pause(milliseconds: 3);
+  public int score = 0;
 
   public Game() {
 
@@ -25,70 +33,80 @@ public class Game {
     timesAvoid = 0;
     updateTitle();
     grid.setBackground("images/map.jpg");
-    grid.setImage(new Location(userRow, 0), userPic);
+    grid.setImage(new Location(userRow, 0), targetPic);
   }
 
   public void play() {
 
     while (!isGameOver()) {
       Grid.pause(100);
-      handleKeyPress();
-      System.out.println("finished hkp");
+
+      handleMousePressed();
+
+      // System.out.println("finished hkp");
       if (msElapsed % 300 == 0) {
-        // this will make move
-        scrollLeft();
-        System.out.println("about to spawn");
+        // System.out.println("about to spawn");
         spawn();
       }
-      System.out.println("finished if");
+      // System.out.println("finished if");
       updateTitle();
-      System.out.println("updated title");
+      // System.out.println("updated title");
       msElapsed += 100;
-      System.out.println("bottom of cycle");
+      // System.out.println("bottom of cycle");
     }
   }
-public void handleMousePressed() {
+
+  public void handleMousePressed() {
 
     // check last key pressed
     Location loc = grid.checkLastLocationClicked();
     System.out.println(loc);
 
-    //if (loc) {
+    String currentPic = grid.getImage(loc);
+    if (targetPic.equals(currentPic)) {
+      // else if(!targetPic.equals(currentPic)&& grid. ){
+      getScore();
     }
 
-
-  public void handleKeyPress() {
-
-    // check last key pressed
-    int key = grid.checkLastKeyPressed();
-    System.out.println(key);
-
-    // set "w" key to move the plane up
-    if (key == 87) {
-      // check case where out of bounds
-
-      // change the field for userrow
-
-      userRow--;
-
-      // shift the user picture up in the array
-      Location loc = new Location(userRow, 0);
-      grid.setImage(loc, "user.gif");
-
-      Location oldLoc = new Location(userRow + 1, 0);
-      grid.setImage(oldLoc, null);
-
-    }
-    // if I push down arrow, then plane goes down
+    // if (loc) {
 
   }
+
+  /*
+   * public void handleKeyPress() {
+   * 
+   * // check last key pressed
+   * int key = grid.checkLastKeyPressed();
+   * System.out.println(key);
+   * 
+   * // set "w" key to move the plane up
+   * if (key == 87) {
+   * // check case where out of bounds
+   * 
+   * // change the field for userrow
+   * 
+   * userRow--;
+   * 
+   * // shift the user picture up in the array
+   * Location loc = new Location(userRow, 0);
+   * grid.setImage(loc, "user.gif");
+   * 
+   * Location oldLoc = new Location(userRow + 1, 0);
+   * grid.setImage(oldLoc, null);
+   * 
+   * }
+   * // if I push down arrow, then plane goes down
+   * 
+   * }
+   */
 
   // populate target
   public void spawn() {
     // spawns random target on the grid
-    for(int i = 0; i < grid.getNumRows(); i++){
-      for(int c = 0; c< grid.getNumCols(); i++){
-
+    for(int r = 0; r < grid.getNumRows(); r++){
+      for(int c = 0; c< grid.getNumCols(); c++){
+        //Object tagetPic;
+        [r][c] = tagetPic;
 
       }
     }
@@ -106,8 +124,9 @@ public void handleMousePressed() {
   }
 
   public int getScore() {
+    score++;
     // score updates if you hit the target
-    return 0;
+    return score;
   }
 
   public void updateTitle() {
@@ -119,8 +138,8 @@ public void handleMousePressed() {
     return false;
   }
 
-  //public boolean accuracy() {
+  // public boolean accuracy() {
 
-    // average from target divided by the shots made.
-  
+  // average from target divided by the shots made.
+
 }
